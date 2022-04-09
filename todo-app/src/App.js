d66a0772468be58330a3c046860b858dca51ff65
Gunnar Jessee <gunnarjessee@gmail.com>
2022-04-09 06:41:37 -0400

    css init

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import TodoCardComponent from "./components/TodoCardComponent";
+import "./App.css"
+function App() {
+  return (
+    
+    <div className=''>
+      <TodoCardComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/components/TodoCardComponent.js
@@ -0,0 +1,17 @@
+
+import './todocard.css'
+
+function TodoCardComponent() {
+    return (
+        <div className='wrapper'>
+            <div className='card'>
+                <div className='card-item'>4/3/22</div>
+                <div className='card-item'>You're my Hero Chris</div>
+                <button className='card-item button'>X</button>
+            </div>
+
+        </div>
+        )
+}
+
+export default TodoCardComponent;
